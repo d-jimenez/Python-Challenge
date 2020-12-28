639424998d7e58bb4108fb2358b2e7ba4633bce4
David Jimenez <david@Davids-MacBook-Air.local>
2020-12-27 18:05:36 -0700

    added script to calculate total number of months, net change and average change to budget data and print it to the terminal.

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,6 +4,9 @@ import csv
 
 csvpath=os.path.join("Resources","budget_data.csv")
 
+date_count=0
+net_gain_loss=float(0)
+
 with open(csvpath) as csvfile:
 
     csvreader=csv.reader(csvfile, delimiter=",")
@@ -16,5 +19,12 @@ with open(csvpath) as csvfile:
     print(csv_header)
 
     for row in csvreader:
+        date_count+=1
+        net_gain_loss+=float(row[1])
+    
+    average_gain_loss=net_gain_loss/date_count
 
-        print(row)
+    print(f"There are a total of {date_count} months in the budget data set.")
+    print(f"The net gain/loss for the budget data is {net_gain_loss} dollars.")
+    print(f"The average gain/loss for the budget data is {average_gain_loss} dollars.")
+
