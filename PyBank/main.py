443af096f3a52f3e6778798611b007ee7ab597fd
David Jimenez <david@Davids-MacBook-Air.local>
2021-01-06 22:14:27 -0700

    updated comments and deleted debugging code.

--- PyBank/main.py
+++ PyBank/main.py
@@ -13,83 +13,65 @@ text_output_path=os.path.join("Analysis","budget_analysis_output.txt")
 dates_list=[]
 #blank list used to store dates in order to cound number of months
 
-# net_gain_loss2=float(0)
 profit_loss_list=[]
 #blank list to store monthly profit/loss number
 
 monthly_change=[]
 #blank list used to store the month over month change starting with profit_loss[1]-profit_loss[0]
+
 with open(csvpath) as csvfile:
 #acts as content manager to close file after all indented code is ran
 
     csvreader=csv.reader(csvfile, delimiter=",")
     #stores the content of csv data into a list of lists
-    # print(csvreader)
 
     csv_header=next(csvreader)
     #itterates through next item within a list and stores the first itterated list as the header
-    # print(f"CSV Header: {csv_header}")
-    # print(csv_header)
 
     for row in csvreader:
         #itterate through the row of data within the csvfile, starting with row index 1 after the header
-        # print(row)
-        # date_count+=1
 
         dates_list.append(row[0])
         #Append the date to the empty dates_list
 
         profit_loss_list.append(float(row[1]))
         #append the profit/losst data into the empty profit_loss_list
-        # net_gain_loss2+=float(row[1])
     
     date_count=(len(dates_list))
     #finds length of the dates_list to determine number of months
-    # print(date_count)
     
     net_gain_loss=sum(profit_loss_list)
     #sum of every months profit/loss to fint net_gain_loss
-    # print(net_gain_loss)
-    # print(net_gain_loss2)
 
     for n in range(1, len(profit_loss_list)):
     #itterates through the profit_loss_list to find the n-(n-1) 
     # month over month change, begining with index 1 of the list
+        
         monthly_change.append(profit_loss_list[n]-profit_loss_list[n-1])
         #append the month over month change to the empty monthl_change list
     
-    # print(monthly_change)
-
     average_monthly_change=sum(monthly_change)/len(monthly_change)
     #calculates the average month over month change
-    # print(round(average_monthly_change,2))
 
     max_change=max(monthly_change)
     #calculates max change for month over month list
-    # print(max(monthly_change))
-
 
     max_index=monthly_change.index(max_change)
     #calculates index of max change for month over month list
-    # print(max_index)
 
     max_date=dates_list[max_index+1]
     #Finds where in the monthly_change list the maximum change occurs
     #need to add 1 to the index because the monthly_change list starts at index 1 and not 0
-    # print(max_date)
 
     min_change=min(monthly_change)
     #calculates min change for month over month list
-    # print(min(monthly_change))
 
     min_index=monthly_change.index(min_change)
     #calculates index of min change for month over month list
-    # print(min_index)
 
     min_date=dates_list[min_index+1]
     #Finds where in the monthly_change list the minimum change occurs
     #need to add 1 to the index because the monthly_change list starts at index 1 and not 0
-    # print(min_date)
 
     with open(text_output_path,"w") as text:
     #opens the text output file, if one does not exist, it will create one
